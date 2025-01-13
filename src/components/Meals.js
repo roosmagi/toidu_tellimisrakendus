a11246930cb30017af6984388e29dcd30d2b2c77
Angela Roosmägi <angela.roosmagi@voco.ee>
2025-01-13 13:53:41 +0200

    Ülesanne 3 - andmete pärimine Meals.js komponendis

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,9 +1,24 @@
 const Meals = () => {
+    
+    async function fetchMeals() {
+        try {
+            const response = await fetch("http://localhost:3001/meals")
+            if (!response.ok) {
+                throw new Error (`HTTP error! status: ${response.status}`)
+            } 
+            const meals = await response.json()
+            console.log(meals)
+        } catch (error) {
+            console.error("Failed to fetch meals", error)
+        } 
+    } 
+
+    fetchMeals()
+    
     return (
         <ul id="meals">
             { 
             <p>Create a list of meals, using fetch data from backend</p>
-                // list of meals
             }
         </ul>
     )
