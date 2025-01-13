75253803f4cab09352e5501b5dfee45a0c2ca90b
Angela Roosmägi <angela.roosmagi@voco.ee>
2025-01-13 13:11:15 +0200

    Ülesanne 1 - Header and Meals komponentide lisamine rakendusele

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Header from "./components/Header"
+import Meals from "./components/Meals"
+
 const App = () => {
  return (
     <>
-     <h1>Food Order App</h1>
+     <Header/>
+     <Meals/>
     </>
   );
 }
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -2,6 +2,7 @@ const Meals = () => {
     return (
         <ul id="meals">
             { 
+            <p>Create a list of meals, using fetch data from backend</p>
                 // list of meals
             }
         </ul>
